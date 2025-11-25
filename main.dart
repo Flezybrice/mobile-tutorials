1a23ee768c706370d63f81f51e888dec66b3294f
Flezybrice <150701386+Flezybrice@users.noreply.github.com>
2025-11-25 23:31:17 +0100

    Calculator app files
    for demo calculator

--- main.dart
+++ main.dart
@@ -1,6 +1,20 @@
-
 import 'package:flutter/material.dart';
-import 'bmi_app.dart';
+import 'calc.dart';
 
-void main() => runApp(const BMIApp());
+void main() => runApp(const MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3:false,
+      ),
+      home: const CalculatorPage(),
+    );
+  }
+}
